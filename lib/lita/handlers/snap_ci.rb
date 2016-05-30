d8ba580fbd6dc780b35a450aae635f573721cd47
Juan Fraire <groteck@gmail.com>
2016-05-30 23:57:21 +0000

    Add custom snap ci http connector

--- lib/lita/handlers/snap_ci.rb
+++ lib/lita/handlers/snap_ci.rb
@@ -1,10 +1,26 @@
+require_relative '../../snap_ci_http'
+
 module Lita
   module Handlers
     class SnapCi < Handler
+      config :user, type: String, required: true
       config :token, type: String, required: true
-      config :repos, type: Hash, default: {}
-      config :default_rooms, type: [Array, String]
+      config :projects, type: Array, required: true
 
+      route(/^snap-ci\s+status$/,
+            :status,
+            help: {'snap-ci' => 'display integration status' })
+
+      def status(response)
+        http_response = http.get
+        response.reply MultiJson.load(http_response.body)
+      end
+
+      private
+
+      def http
+        SnapCi::Http.new(config)
+      end
 
       Lita.register_handler(self)
     end
